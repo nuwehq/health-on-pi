e588616274b3d567dddca205f325fa03f65029c9
Steve Schofield <stevebschofield@gmail.com>
2015-04-15 13:59:06 +0100

    implement nuwe login with oauth strategy

--- app.js
+++ app.js
@@ -1,16 +1,72 @@
 var request = require('request');
 var express = require('express');
+var passport = require('passport');
+var NuweStrategy = require('passport-nuwe').Strategy;
+var express = require('express'); 
+var util = require( 'util' )
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var cookieSession = require('cookie-session'); 
+var session = require('express-session');
 var mustacheExpress = require('mustache-express');
 var app = express();
+var server = require( 'http' ).createServer( app ) 
 
-app.use(express.static(__dirname + '/public'));
+var NUWE_CLIENT_ID = "a17594f29a795a031dd4905f03e0f929b1504e34d79d8e0263938d63d359a932";
+var NUWE_CLIENT_SECRET = "d8222c1b1d0a0daf86614d9c2c381465891fb147645a155251cec6c52fa0ffd3";
 
+// Passport session setup.
+//   To support persistent login sessions, Passport needs to be able to
+//   serialize users into and deserialize users out of the session.  Typically,
+//   this will be as simple as storing the user ID when serializing, and finding
+//   the user by ID when deserializing.  However, since this example does not
+//   have a database of user records, the complete Google profile is
+//   serialized and deserialized.
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+// Configure App
 app.engine('mustache', mustacheExpress());
-
 app.set('view engine', 'mustache');
 app.set('views', __dirname + '/views');
+app.use( cookieParser()); 
+app.use( bodyParser.json());
+app.use( bodyParser.urlencoded({
+	extended: true
+}));
+app.use( passport.initialize());
+app.use( passport.session());
 
-app.get('/', function( req, res ){
+passport.use(new NuweStrategy({
+    clientID: NUWE_CLIENT_ID,
+    clientSecret: NUWE_CLIENT_SECRET,
+    callbackURL: "http://127.0.0.1:3000/auth/nuwe/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    User.findOrCreate({ NuweId: profile.id }, function (err, user) {
+      return done(err, user);
+    });
+  }
+));
+
+
+
+app.get('/auth/nuwe',
+  passport.authenticate('nuwe'));
+
+app.get('/auth/nuwe/callback', 
+  passport.authenticate('nuwe', { failureRedirect: '/sign_in' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
+
+app.get('/', ensureAuthenticated, function( req, res ){
   res.render('dashboard');
 });
 
@@ -23,7 +79,7 @@ app.get('/sign_up', function( req, res ){
 });
 
 app.get('/sign_in', function( req, res ){
-	res.render('sign_in')
+	res.render('sign_in');
 });
 
 app.get('/log_out', function( req, res ){
@@ -31,7 +87,9 @@ app.get('/log_out', function( req, res ){
 });
 
 
-var server = app.listen(3000, function () {
+
+
+server.listen(3000, function () {
 
   var host = server.address().address;
   var port = server.address().port;
@@ -39,3 +97,13 @@ var server = app.listen(3000, function () {
   console.log('Hopi running at http://%s:%s', host, port);
 
 });
+
+// Simple route middleware to ensure user is authenticated.
+//   Use this route middleware on any resource that needs to be protected.  If
+//   the request is authenticated (typically via a persistent login session),
+//   the request will proceed.  Otherwise, the user will be redirected to the
+//   login page.
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/sign_in')
+}
